aa0da6779b5c8796c90175d281b63e806e565c61
hades102zues <joshua.kar@hotmail.com>
2025-11-05 21:53:06 -0400

    now we cluster, and store cluster information on the csv

--- analyze_samples.py
+++ analyze_samples.py
@@ -2,6 +2,8 @@ import os
 import json
 import pandas as pd
 from pathlib import Path
+from sklearn.metrics import pairwise_distances
+from sklearn_extra.cluster import KMedoids
 
 
 def check_for_virustotal_db(filename):
@@ -10,11 +12,116 @@ def check_for_virustotal_db(filename):
             virus_total_sample_db = json.load(f)
         return virus_total_sample_db
     else: 
+        print("WARNING VIRUS TOTAL DB DOES NOT EXIST!")
         return {}
     
+def check_for_tatics_db(filename):
+    if os.path.exists(filename):
+        with open(filename, "r") as f:
+            tatics_db = json.load(f)
+        return tatics_db
+    else: 
+        print("WARNING TATICS DB DOES NOT EXIST!")
+        return {}
+    
+def check_for_techn_db(filename):
+    if os.path.exists(filename):
+        with open(filename, "r") as f:
+            techn_db = json.load(f)
+        return techn_db
+    else: 
+        print("WARNING TECHNIQUES DB DOES NOT EXIST!")
+        return {}
+
+
+def cluster_data(tatics_lookup_db, 
+                 techn_lookup_db, 
+                 sample_families,
+                 one_hot_technique_matrix, 
+                 cluster_top_techniques_file,
+                 cluster_top_tactics_file):
+
+    # remove the families to prevent bias
+    pairwise_df = one_hot_technique_matrix.drop(columns=sample_families, errors="ignore")
+    
+    # compute the jaccard distances
+    jaccard = pairwise_distances(pairwise_df.to_numpy(), metric="jaccard")
+
+    # Run K-Medoids
+    kmed = KMedoids(n_clusters=4, metric="precomputed", random_state=42)
+    kmed.fit(jaccard)
+
+    # Add back our cluster labels
+    one_hot_technique_matrix["cluster"] = kmed.labels_
+
+
+##==== cluster tactics
+    # determine the Tatics columns present in the matrix
+    tactic_cols = [col for col in one_hot_technique_matrix.columns if col.startswith("TA")]
+
+    # group by on the cluster value and then get a mean for each Tactic in the cluster
+    cluster_tactic_profiles = one_hot_technique_matrix.groupby("cluster")[tactic_cols].mean()
+
+    cluster_name = {} # stores names in csv
+    cluster_tactics = {} # collects items in the code
+
+    for c in cluster_tactic_profiles.index:
+        # rotate so now each cluster index is the column
+        # each row is a tatic and the value amount under each cluster column
+        # grab the top 3 tactics per cluster
+        top_tactics_per_cluster = cluster_tactic_profiles.T.nlargest(3, c)
+
+        # convert the IDs to names and store them
+        named_tatics = [tatics_lookup_db.get(ta, ta) for ta in top_tactics_per_cluster.index]
+        cluster_name[c] = " + ".join(named_tatics)
+
+        cluster_tactics[c] = {}
+        cluster_tactics[c]["id"] = top_tactics_per_cluster.index.to_list()
+        cluster_tactics[c]["name"] = named_tatics
+   
+    # send the data to the csv
+    one_hot_technique_matrix["cluster_name"] = one_hot_technique_matrix["cluster"].map(cluster_name)
+    one_hot_technique_matrix.to_csv("./vault/one_hot_matrix.csv", index=False)
+
+    with open(cluster_top_tactics_file, "w") as f:
+        json.dump(cluster_tactics, f, indent=2)
+
+
+#=====cluster top 5 techniques
+    # determine the technique columns present in the matrix
+    techn_cols = [col for col in one_hot_technique_matrix.columns if col.startswith("T1")]
+
+    # group by on the cluster value and then get a mean for each techniques in the cluster
+    cluster_techn_profiles = one_hot_technique_matrix.groupby("cluster")[techn_cols].mean()
+
+
+    
+    cluster_techniques = {}
+
+    for c in cluster_techn_profiles.index:
+        top_techn_per_cluster = cluster_techn_profiles.T.nlargest(5, c)
+        named_techniques = [techn_lookup_db.get(tech, tech) for tech in top_techn_per_cluster.index]
+        cluster_techniques[c] = {}
+        cluster_techniques[c]["id"] = top_techn_per_cluster.index.to_list()
+        cluster_techniques[c]["name"] = named_techniques
+    
+    # save samples to file
+    with open(cluster_top_techniques_file, "w") as f:
+        json.dump(cluster_techniques, f, indent=2)
+
+    return [cluster_tactics, cluster_techniques]
+    
+
 def main():
     virus_total_file = Path("vault") / "virus_total_info.json"
+    tatics_name_file = Path("vault") / "tactic_info.json"
+    techn_name_file = Path("vault") / "technique_info.json"
+    cluster_top_techniques_file = Path("vault") / "cluster_technique_info.json"
+    cluster_top_tactics_file = Path("vault") / "cluster_tactic_info.json"
+
     virus_total_lookup_db = check_for_virustotal_db(virus_total_file)
+    tatics_lookup_db = check_for_tatics_db(tatics_name_file)
+    techn_lookup_db = check_for_techn_db(techn_name_file)
 
     if len(virus_total_lookup_db) < 1:
         print("***YOU HAVE NO VIRUS TOTAL DATA***")
@@ -22,19 +129,23 @@ def main():
     
     sample_hashes = []
     sample_traits = []
+    sample_families = []
+    temp = set()
 
     for virus_hash, info in virus_total_lookup_db.items():
         sample_hashes.append(virus_hash)
 
-        # Combine family name with the list of techniques
+        # Combine family name + tactics + with the list of techniques
         combined_traits = [info["family"]] + info["tactic_ids"]+info["flattened_technique_ids"]
         sample_traits.append(combined_traits)
+        temp.add(info["family"])
 
        
     data = {
         "sample": sample_hashes,
         "traits": sample_traits
     }
+    sample_families = list(temp)
 
     # loading a dataframe
     df = pd.DataFrame(data)
@@ -47,11 +158,21 @@ def main():
     
     # this will arrange each row in order of occurenence within sample_hashes.
     one_hot_technique_matrix = one_hot_technique_matrix.reindex(sample_hashes)
-    
+
+    # save our dataframe to file
     matrix_file = Path("vault") / "one_hot_matrix.csv"
     one_hot_technique_matrix.to_csv(matrix_file, index=False)
+
+    cluster_data(tatics_lookup_db, 
+                 techn_lookup_db, 
+                 sample_families, 
+                 one_hot_technique_matrix, 
+                 cluster_top_techniques_file,
+                 cluster_top_tactics_file)
+
+
     
 
-    # bunch of ml analysis traits or whatever 
+    
 if __name__ == "__main__":
     main()
